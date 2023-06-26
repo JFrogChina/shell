20eecfbe4cb6e97d84883611aaa34d43098bb811
someone <someone@example.com>
2023-06-26 17:28:16 +0800

    c

--- checknginx-cheng/1-nginx.sh
+++ checknginx-cheng/1-nginx.sh
@@ -3,7 +3,7 @@
 >results
 
 # read gpwxxx.log
-ls -v gpw* | while read log_file;do
+ls -v nginx* | while read log_file;do
     
     echo $log_file
 
@@ -11,13 +11,13 @@ ls -v gpw* | while read log_file;do
     # then extracts a specific range of characters using cut
     # and finally sorts and removes duplicates using sort and uniq. 
     # The resulting hours are stored in the variable hours.
-    hours=$(awk '(print $4)' $log_file | cut -c 2-15 | sort uniq)
+    hours=$(awk '{print $4}' $log_file | cut -c 2-15 | sort |uniq)
 
     echo "$hours" | while read hour;do
         
         # PUT
         put_count=`cat $log_file| grep $hour | grep PUT |wc -l`
-        if [ $put_count -eg 0 ];then
+        if [ $put_count -eq 0 ];then
             put_bytes=0
         else
             # This line extracts the byte values associated with PUT operations in the log file for the current hour using grep and awk. 
@@ -28,12 +28,12 @@ ls -v gpw* | while read log_file;do
         
         # GET
         get_count=`cat $log_file| grep $hour | grep GET |wc -l`
-        if [ $get_count -eg 0 ];then
+        if [ $get_count -eq 0 ];then
             get_bytes=0
         else
             get_bytes=`cat $log_file| grep $hour | grep GET |awk '{sum += $10} END {print sum}'`
         fi
-        echo "$hour $put_count $put_bytes $get_count $get_bytes" >> results
+        echo "$hour $put_count $put_bytes $get_count $get_bytes" >> results.log
     done
     
 done
---
+++ checknginx/README.md
@@ -0,0 +1,18 @@
+IP Address: The IP address of the client making the request.
+User: A dash (-) indicates that no user information is available.
+Timestamp: The date and time when the request was made, in the format [day/month/year:hour:minute:second +timezone].
+
+HTTP Method: The HTTP method used for the request (e.g., GET, POST, PUT).
+Requested Resource: The path of the requested resource within the server.
+HTTP Version: The version of the HTTP protocol used for the request.
+Response Status Code: The HTTP status code returned by the server to indicate the success or failure of the request (e.g., 200 for a successful request).
+
+Response Size: The size of the response in bytes.
+
+Referrer: A dash (-) indicates that no referrer information is available.
+User-Agent: The user agent string that identifies the client software making the request.
+
+Request Time: The time taken by the server to process the request, in seconds.
+
+Upstream Address: The IP address and port number of the upstream server handling the request.
+Client IP: The IP address of the client making the request.
